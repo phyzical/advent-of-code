1105493e51fb6219da310368704742eb288ff37a
jack <jack.carpenter@agworld.com>
2021-01-19 17:03:59 +0800

    23 start

--- 2020/23.1.rb
+++ 2020/23.1.rb
@@ -3,19 +3,33 @@ require_relative 'helpers.rb'
 module DayTwentyThreePartOne
   module_function
 
-  def solve(file)
-    inputs = prepare_inputs(file)
+  def solve(string, moves)
+    inputs = prepare_inputs(string)
+    perform_moves(inputs, moves)
   end
 
-  def prepare_inputs(file)
-    Helpers
-    .split_inputs_by_line(Helpers.read_file(file))
+  def perform_moves(inputs, moves)
+    move = 0
+    current_cup_index = 0
+    current_cup = inputs[0]
+    destination_label = current_cup - 1
+    while moves > move
+      ##  pluck 3 to the right clockwise
+      destination_cup_index = nil
+      while destination_cup_index.nil?
+        destination_label -= 1
+        destination_label = inputs.max if destination_label < inputs.min
+        destination_cup_index = inputs.find(destination_label)
+      end
+      ## place pluck clockwise into inputs
+    end
+    inputs
+  end
+
+  def prepare_inputs(string)
+    string.split('').map(&:to_i)
   end
 end
 
-
-
-
-ap DayTwentyThreePartOne.solve(__dir__ + '/23test.txt')
-
-#ap DayTwentyThreePartOne.solve(__dir__ + '/23.txt')
+ap DayTwentyThreePartOne.solve('389125467', 100)
+# ap DayTwentyThreePartOne.solve('418976235',100)
